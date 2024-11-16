99257a1b1925614912b178f1bd0bc8a72f655204
市川恭佑 <github@mail.ebi-yade.com>
2024-11-16 22:52:50 +0900

    Add t.Helper() to avoid confusion

--- cases/errors.go
+++ cases/errors.go
@@ -9,17 +9,20 @@ import (
 type ErrorCheck func(*testing.T, error)
 
 func NoError(t *testing.T, err error) {
+	t.Helper()
 	require.NoError(t, err)
 }
 
 func ErrorIs(target error) ErrorCheck {
 	return func(t *testing.T, err error) {
+		t.Helper()
 		require.ErrorIs(t, err, target)
 	}
 }
 
 func ErrorAs(target error) ErrorCheck {
 	return func(t *testing.T, err error) {
+		t.Helper()
 		require.ErrorAs(t, err, target)
 	}
 }
